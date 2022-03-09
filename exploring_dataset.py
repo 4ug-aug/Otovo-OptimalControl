2910d912b8248760e127d215c0b9b8f73e55d45b
4ug-aug <augusttollerup@hotmail.com>
2022-03-09 13:16:15 +0100

    Refact: Moved plot_timslot() into a seperate function, and added __name__ = main

--- exploring_dataset.py
+++ exploring_dataset.py
@@ -12,30 +12,36 @@ import datetime as dt
 import pytz
 
 df_main = pd.read_csv('sample-csv', delimiter=';', header=0)
-df = df_main.drop(['id'], axis=1)
 
 
-def dtextract(x):
-    df[x] = pd.to_datetime(df[x], utc=True)
-    df['year'] = df[x].dt.year
-    df['month'] = df[x].dt.month
-    df['hour'] = df[x].dt.hour
-    df['weekday'] = df[x].dt.weekday
+def plot_timeslot():
+    df_copy = df_main.copy()
+    df_copy = df_main.drop(['id'], axis=1)
+
+    def dtextract(x):
+        df_copy[x] = pd.to_datetime(df_copy[x], utc=True)
+        df_copy['year'] = df_copy[x].dt.year
+        df_copy['month'] = df_copy[x].dt.month
+        df_copy['hour'] = df_copy[x].dt.hour
+        df_copy['weekday'] = df_copy[x].dt.weekday
 
 
-dtextract('timeslot')
+    dtextract('timeslot')
 
-fig, axs = plt.subplots(figsize=(12, 4))
-df2 = df[df['type']=='production']
-df3 = df[df['type']=='consumption']
-df4 = df[df['type']=='elcert']
-plt.plot(df2.groupby(df2['weekday'])["num_kwh"].mean(), color = 'green')
-plt.plot(df3.groupby(df3['weekday'])["num_kwh"].mean(), color = 'red')
-plt.plot(df4.groupby(df4['weekday'])["num_kwh"].mean(), color = 'blue')
+    fig, axs = plt.subplots(figsize=(12, 4))
+    df2 = df_copy[df_copy['type']=='production']
+    df3 = df_copy[df_copy['type']=='consumption']
+    df4 = df_copy[df_copy['type']=='elcert']
+    plt.plot(df2.groupby(df2['weekday'])["num_kwh"].mean(), color = 'green')
+    plt.plot(df3.groupby(df3['weekday'])["num_kwh"].mean(), color = 'red')
+    plt.plot(df4.groupby(df4['weekday'])["num_kwh"].mean(), color = 'blue')
 
 
-df2.groupby(df2['weekday'])["num_kwh"].mean().plot(kind='bar', rot=0, ax=axs)
+    df2.groupby(df2['weekday'])["num_kwh"].mean().plot(kind='bar', rot=0, ax=axs)
 
+
+if __name__ == "__main__":
+    plot_timeslot()
 
 
 
