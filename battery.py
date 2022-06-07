14038141402d000127c3a1ba49adbfaf45788e35
Andreas Fiehn <andreas@lyhnefiehn.dk>
2022-06-07 14:36:28 +0200

    Fix/Refact: Merged charge/discharge into one, test code, fixed surplus deficit reset

--- battery.py
+++ battery.py
@@ -1,54 +1,47 @@
 class Battery:
-    def __init__(self, max_capacity = 13.5, current_capacity = 0, over_under_charge = 0) -> None:
+    def __init__(self, max_capacity = 13.5, current_capacity = 0, surplus_deficit = 0) -> None:
         self.max_capacity = max_capacity
         self.current_capacity = current_capacity
-        self.over_under_charge = over_under_charge
+        self.surplus_deficit = surplus_deficit
 
-    def charge(self, amount: float) -> None:
-        """Charge battery
-
-        Args:
-            amount (float): amount
-        """
+    def charge(self, amount: float) -> float:
 
         self.current_capacity += amount
+        self.surplus_deficit = 0
         
-        if self.current_capacity > self.max_capacity:
-            yield_ = self.current_capacity - self.max_capacity
+        # We charge the battery
+        if self.current_capacity >= self.max_capacity:
+            self.surplus_deficit = self.current_capacity - self.max_capacity
             self.current_capacity = self.max_capacity
 
-            self.over_under_charge = yield_
-        
-    def discharge(self, amount: float) -> float:
-        """Discharge battery
-            
-            Args:
-                amount (float): amount
-        """
 
-        self.current_capacity += amount
-        
-        if self.current_capacity < 0:
+        # We discharge the battery
+        elif self.current_capacity <= 0:
             # Return the amount of energy that was not discharged
-            yield_ = self.current_capacity
+            self.surplus_deficit = self.current_capacity
             self.current_capacity = 0
-        
-            self.over_under_charge = yield_
-        
-        # return amount
 
-    def get_over_under_charge(self) -> float:
-        return self.over_under_charge
+
+    def get_surplus_deficit(self) -> float:
+        return self.surplus_deficit
     
     def get_current_capacity(self) -> float:
         return self.current_capacity
     
     def get_percentage(self) -> float:
         return self.current_capacity / self.max_capacity * 100
+    
 
+if __name__ == "__main__":
+    battery = Battery(max_capacity = 10, current_capacity=5)
 
-def logic():
-    """_summary_
-    """
-
-    return None
+    # Test battery charging and discharging
+    print(battery.get_current_capacity(), battery.get_surplus_deficit())
+    battery.charge(2)
+    print(battery.get_current_capacity(), battery.get_surplus_deficit())
+    battery.charge(5)
+    print(battery.get_current_capacity(), battery.get_surplus_deficit())
+    battery.charge(-9)
+    print(battery.get_current_capacity(), battery.get_surplus_deficit())
+    battery.charge(-10)
+    print(battery.get_current_capacity(), battery.get_surplus_deficit())
