fa3f8fb4ea7002d1b0d29f9d51958f70770472b8
4ug-aug <augusttollerup@hotmail.com>
2022-06-13 11:18:03 +0200

    Feat: Calcuate cost of DP based on actions

---
+++ calculate_cost_dp.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+import json
+import matplotlib.pyplot as plt
+
+meter_data = pd.read_csv("data/gridtx-dump-AGGREGATED-CLEANED-THRESHOLD-COVERAGE100-NORMALIZED-PROD.csv")
+
+timeline = meter_data[meter_data["meter_id"] == "e882f9a7-f1de-4419-9869-7339be303281"]["timeslot"]
+
+# Read data/dp/actions_small_graph_new_meter_id.txt
+with open('data/dp/actions_small_graph_new_meter_id.txt', 'r') as f:
+    actions = f.read()
+
+# Load data/hour_lookup_price_dict.json
+with open('data/hour_lookup_price_dict.json', 'r') as f:
+    hour_lookup_price_dict = json.load(f)
+
+# Convert string to list of lists
+actions = literal_eval(actions)
+
+# loop through actions
+cost = 0
+cost_list = []
+for idx, action in enumerate(actions):
+    # Get day and hour of action
+    day = str(pd.to_datetime(timeline.iloc[idx]).day)
+    hour = str(pd.to_datetime(timeline.iloc[idx]).hour)
+    # Get price of hour
+    price = hour_lookup_price_dict[day][hour]
+
+    # Get cost of action
+    buy = action[0]
+    sell = action[1]
+    cost += buy * price - sell * price*0.1
+    cost_list.append(cost)
+
+print(f"Cost: ", cost)
