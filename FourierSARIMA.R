5536b2a77a094f4230184b1ab0065e035ba0f27c
vidisha04 <99336979+vidisha04@users.noreply.github.com>
2022-06-14 14:51:08 +0200

    Update FourierSARIMA.R

--- FourierSARIMA.R
+++ FourierSARIMA.R
@@ -4,25 +4,17 @@ library(grid)
 library(TSstudio)
 library(TTR)
 library(graphics)
+library(xts)
+library(reshape2)
 
 data = read.csv('C:/Users/vidis/OneDrive/Desktop/Summer2022/Project Work/gridtx-dump-AGGREGATED-CLEANED-THRESHOLD-COVERAGE100-NORMALIZED-CONS.csv', sep=',', header=TRUE)
-data = data[data$meter_id=="28ba7f57-6e83-4341-8078-232c1639e4e3",]
+data = data[data$meter_id=="e882f9a7-f1de-4419-9869-7339be303281",]
 data = data[order(as.POSIXlt(data$timeslot, format="%Y-%m-%d %H:00:00+02")),]
-train <- data[22847:23567,]
-test <- data[23568:23592,]
 
-firstHour_train <- 24*(as.Date("2019-02-18 00:00:00")-as.Date("2019-1-1 00:00:00")) # As suggested by Mark S
-firstHour_test <- 24*(as.Date("2019-03-30 00:00:00")-as.Date("2019-1-1 00:00:00")) # As suggested by Mark S
+data = data[(data$timeslot>='2017-09-15 00:00:00+02'),]
+data = data[order(as.POSIXlt(data$timeslot, format="%Y-%m-%d %H:00:00+02")),]
 
-train.ts <- ts(train$num_kwh_normalized, start=c(2019,firstHour_train), frequency=24*365.25)
-test.ts <- ts(test$num_kwh_normalized, start=c(2019,firstHour_test), frequency=24*365.25)
-
-#multiple seasonality
-y <- msts(train$num_kwh_normalized, seasonal.periods= c(24,7*24))
-fit <- tbats(y, use.box.cox=FALSE, use.arma.errors = FALSE, use.parallel = TRUE)
-fc <- forecast(fit)
-
-## frequency day, initial size = 720 ()
+## frequency by day, initial size = 1440
 
 iniTrain<- 1440
 step_size <- 24
@@ -32,18 +24,19 @@ forecasted <- c()
 f_upper_bounds <- c()
 f_lower_bounds <- c()
 
-for (i in 0:953){
+for (i in 0:10){
   
   d <- (iniTrain+step_size*i)
-  train <- data[d:((iniTrain-1)+d),]
-  test <- data[(iniTrain+d):(iniTrain+23+d),]
+  e <- iniTrain-1
+  train <- data[(d-e):((iniTrain-1)+d-e),]
+  test <- data[(iniTrain+d-e):(iniTrain+step_size-1+d-e),]
   
-  train.ts <- ts(train$num_kwh_normalized, frequency=24)
+  train.ts <- ts(train$num_kwh, frequency=24)
   
-  fit <- auto.arima(train.ts, seasonal=TRUE, trace=FALSE, D=1)
+  fit <- auto.arima(train.ts, seasonal=TRUE, trace=TRUE, D=1)
   fc <- forecast(fit, h=24, level=c(95))
   
-  x <- data.frame(cbind(test$num_kwh_normalized, fc$mean, fc$lower,fc$upper))
+  x <- data.frame(cbind(test$num_kwh, fc$mean, fc$lower,fc$upper))
   p = ggplot()+geom_line(data=x, aes(x = seq(1,24), y=x[,1], colour='actual'))+geom_line(data=x, aes(x = seq(1,24),y=x[,2], colour='forecasted'))+geom_line(data=x, aes(x = seq(1,24),y=x[,3], colour='lower bound'))+geom_line(data=x, aes(x = seq(1,24),y=x[,4], color='upper bound'))+labs(color = "Value")
   print(p)
   mse <- (1/24)*sum((x[,1]-x[,2])^2)
@@ -59,5 +52,17 @@ for (i in 0:953){
   
 }
 
-print(mean(MSEs))
+a <- data.frame(index=seq(1,length(data[1:length(forecasted),3])), actual= data[1:length(forecasted),12], forecasted=forecasted, UB=f_upper_bounds, LB=f_lower_bounds)
+p = ggplot(a, aes(index)) + 
+  geom_line(aes(y=actual, colour="actual")) +
+  geom_line(aes(y=forecasted, colour="forecasted"))+
+  geom_ribbon(aes(ymin=LB, ymax=UB), alpha=0.2)+
+  labs(x='index', y='norm_consumption', color='Legend')+
+  scale_color_manual(name='Values', values = c('actual'='orange', 'forecasted'='blue') )
+print(p)
+
+print(mean(sqrt(MSEs)))
+
+plot(sqrt(MSEs))
+
 
