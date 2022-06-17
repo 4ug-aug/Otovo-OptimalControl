47f6549855ce253a9c2b8aedc3b7d158de6fe7fa
4ug-aug <augusttollerup@hotmail.com>
2022-06-17 13:11:35 +0200

    Feat: Added price calculation of list of actions from DP

--- calculate_cost_dp.py
+++ calculate_cost_dp.py
@@ -13,7 +13,7 @@ with open('data/dp/actions_small_graph_new_meter_id.txt', 'r') as f:
     actions = f.read()
 
 # Load data/hour_lookup_price_dict.json
-with open('data/hour_lookup_price_dict.json', 'r') as f:
+with open('data/nordpool_hour_lookup_price_dict.json', 'r') as f:
     hour_lookup_price_dict = json.load(f)
 
 # Convert string to list of lists
@@ -24,15 +24,16 @@ cost = 0
 cost_list = []
 for idx, action in enumerate(actions):
     # Get day and hour of action
-    day = str(pd.to_datetime(timeline.iloc[idx]).day)
-    hour = str(pd.to_datetime(timeline.iloc[idx]).hour)
+    timestamp = pd.to_datetime(timeline.iloc[idx])
+    day = str(358 if timestamp.dayofyear > 358 else timestamp.day_of_year)
+    hour = str(timestamp.hour)
     # Get price of hour
     price = hour_lookup_price_dict[day][hour]
 
     # Get cost of action
     buy = action[0]
     sell = action[1]
-    cost += buy * price + sell * price*0.1
+    cost += buy * price - sell * price*0.1
     cost_list.append(cost)
 
 print(f"Cost: ", cost)
